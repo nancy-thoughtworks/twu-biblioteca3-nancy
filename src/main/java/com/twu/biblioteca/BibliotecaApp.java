ee8f176e117137d99b6d3b221273d1a6ab7dede8
Nancy Lee <nancylee@Nancys-MacBook-Pro.local>
2019-08-19 18:02:26 +1000

    run main

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,12 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        run();
+    }
+
+    public static void run() {
+        WelcomeMessage msg = new WelcomeMessage();
+        msg.printMessage();
+
     }
 }
